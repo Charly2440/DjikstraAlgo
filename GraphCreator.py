42360cd439e00e0d69bf87a164c8dd63ecc758fb
Carlos Villagran <cjvillagranmata@gmial.com>
2024-05-04 15:46:18 -0600

    graficador de grafos

--- GraphCreator.py
+++ GraphCreator.py
@@ -1,4 +1,6 @@
 import networkx as nx
+from matplotlib import pyplot as plt
+import heapq
 class GraphCreator:
     def __init__(self, rootData):
         self.rootData = rootData
@@ -34,8 +36,35 @@ class GraphCreator:
         return self.graph.edges()
 
     def createGraph(self):
-        self.parseData()
         self.createNodes()
         self.createEdges()
 
         return self.graph
+
+    #Algoritmo de grafico utilizando matplotlib obtenido de la documentacion ofical:
+    #https://networkx.org/documentation/stable/auto_examples/drawing/plot_weighted_graph.html
+    def graphicGraph(self):
+        # Draw the graph
+        elarge = [(u, v) for (u, v, d) in self.graph.edges(data=True)]
+        esmall = [(u, v) for (u, v, d) in self.graph.edges(data=True)]
+
+        pos = nx.spring_layout(self.graph, seed=7)  # positions for all nodes - seed for reproducibility
+
+        # nodes
+        nx.draw_networkx_nodes(self.graph, pos, node_size=700)
+
+        # edges
+        nx.draw_networkx_edges(self.graph, pos, edgelist=elarge, width=2)
+        nx.draw_networkx_edges(self.graph, pos, edgelist=esmall, width=2, alpha=0.5, edge_color="b", style="dashed")
+
+        # node labels
+        nx.draw_networkx_labels(self.graph, pos, font_size=15, font_family="sans-serif")
+        # edge weight labels
+        edge_labels = nx.get_edge_attributes(self.graph, "weight")
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels)
+
+        ax = plt.gca()
+        ax.margins(0.01)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
